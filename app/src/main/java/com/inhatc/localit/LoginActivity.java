a159b3428cc8226b030d9bfc9813e4d021baebf7
chanuk <wook9271@gmail.com>
2025-07-26 02:49:08 +0900

    setting

--- app/src/main/java/com/inhatc/localit/LoginActivity.java
+++ app/src/main/java/com/inhatc/localit/LoginActivity.java
@@ -68,17 +68,22 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    @Override//구글 로그인
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account);
-            } catch (ApiException e) {
-                Log.w("Google Sign In", "Google sign in failed", e);
+            if (data != null) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    firebaseAuthWithGoogle(account);
+                } catch (ApiException e) {
+                    Log.w("Google Sign In", "Google sign in failed", e);
+                    Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Log.e("Google Sign In", "data is null");
             }
         }
     }
